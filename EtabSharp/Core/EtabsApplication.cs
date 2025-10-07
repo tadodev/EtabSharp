9146d40b1f4fc90ba4b85833d9bcab5d603690d0
Tu Do Thanh <dothanhtupt@gmail.com>
2025-10-08 00:21:08 +0700

    add initial holder

---
+++ EtabSharp/Core/EtabsApplication.cs
@@ -0,0 +1,23 @@
+﻿using ETABSv1;
+
+namespace EtabSharp.Core;
+
+/// <summary>
+/// Entry point for ETABS API operations
+/// </summary>
+public class EtabsApplication: IDisposable
+{
+    public cSapModel Model { get; private set; }
+    public bool IsConnected { get; private set; }
+
+    public EtabsApplication()
+    {
+        
+       
+    }
+
+    public void Dispose()
+    {
+       
+    }
+}
