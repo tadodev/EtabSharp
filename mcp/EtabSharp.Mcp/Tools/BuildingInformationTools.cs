dc587484b026ee6f9299b592d86f40f516ff5d52
Thanh Tu Do <dothanhtupt@gmail.com>
2025-11-14 18:05:07 +0700

    adding place holder mcp

---
+++ mcp/EtabSharp.Mcp/Tools/BuildingInformationTools.cs
@@ -0,0 +1,14 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace EtabSharp.Mcp.Tools;
+
+internal class BuildingInformationTools
+{
+    [McpServerTool]
+    [Description("Get building information like stories, material, section used and loading types")]
+    public int GetBuildingInformation()
+    {
+        return Random.Shared.Next(1, 100);
+    }
+}
